df20da2825593c3b03361889587634bd6fe2b3a0
Rithvik Nishad <rithvikn2001@gmail.com>
2022-03-31 04:43:28 +0530

    Adds SR Flip Flop

---
+++ Flip Flops/SR Flip Flop/SRFF.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+
+/*
+Truth table of SR Flip Flop
+
+  S R     CLK     Q   Q`  Remarks
+  ================================
+  0 0   RISING    Q   Q`  retain
+  1 1   RISING    0   1   toggle
+  1 1   RISING    1   0   toggle
+  1 1   RISING    ?   ?   INVALID
+  x x   FALLING   Q   Q`  retain
+
+*/
+
+module SRFF(
+    input S,        // Set
+    input R,        // Reset
+    input clk,      // Clock
+    output reg Q,   // Output of SR Flip Flop
+    output Qb       // Complimented output of SR Flip Flop
+);
+  // Initialize Q with 0
+  initial Q = 0;
+  
+  // Assign Qb to always be compliment of Q.
+  assign Qb = ~Q;
+  
+  // Whenever there is reset or rising edge in clock signal...
+  always @(posedge clk) begin
+      if (S) Q = 1'b1;
+      if (R) Q = 1'b0;
+  end
+endmodule
---
+++ Flip Flops/SR Flip Flop/SRFF_tb.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+module SRFF_tb;
+  reg S, R, clk;
+  wire Q, Qb;
+  
+  integer i;
+  
+  SRFF uut(S, R, Q, Qb);
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    
+    for (i = 0; i < 6; i = i + 1) begin
+      R = i[2]; S = i[1]; clk = i[0]; #20;
+    end
+  end
+endmodule
